bf6892139a36f5713000a3432b78deb5bfc86862
Bohdan <bodyaborzy@gmail.com>
2025-01-24 12:46:53 +0000

    setup router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {fab} from "@fortawesome/free-brands-svg-icons";
 
-createApp(App).mount('#app')
+library.add(faBars, fab);
+
+
+createApp(App).component('fa',FontAwesomeIcon).use(router).mount('#app')
+
---
+++ src/views/Article.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="header">
+    <Header />
+  </div>
+
+  <div class="content">
+
+  </div>
+
+
+  <div class="footer">
+    <Footer />
+  </div>
+</template>
+
+
+<script >
+import Footer from "@/components/Footer.vue";
+import Header from "@/components/Header.vue";
+
+
+
+export default {
+  name: 'article',
+  components: {
+
+
+    Header,
+
+    Footer,
+
+  },
+}
+</script>
+
+
+<style >
+* {
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+  list-style: none;
+
+
+}
+
+.content{
+  margin: 50px 0 0 0;
+}
+</style>
